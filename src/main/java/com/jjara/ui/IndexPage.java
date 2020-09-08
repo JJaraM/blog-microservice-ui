ab27d4f7260f28239d7adc7b813e6e2aa36c915a
JJaraM <jonathan.jara.morales@gmail.com>
2020-09-08 09:30:27 +0100

    Add gitignore

---
+++ src/main/java/com/jjara/ui/IndexPage.java
@@ -0,0 +1,20 @@
+package com.jjara.ui;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class IndexPage {
+
+    private final static String filePath = "public/index.html";
+    private static String index = null;
+
+    public static String getIndex(final String title, final String description, final String stackTech) {
+        if (index == null) {
+            var classLoader = IndexPage.class.getClassLoader();
+            var inputStream = classLoader.getResourceAsStream(filePath);
+            index = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+        }
+        return index;
+    }
+}
